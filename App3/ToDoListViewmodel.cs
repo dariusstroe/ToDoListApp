4cb78a1e40967e6fdddaad1f86bdf13b78e899ce
Darius <72557469+dariusstroe@users.noreply.github.com>
2022-02-20 17:29:27 +0200

    Add project files.

---
+++ App3/ToDoListViewmodel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public class ToDoListViewmodel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public ToDoListViewmodel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("todo 1", false));
+            TodoItems.Add(new TodoItem("todo 2", false));
+            TodoItems.Add(new TodoItem("todo 3", false));
+        }
+
+        public ICommand AddToDoCommand => new Command(AddToDoItem);
+        public string NewToDoInputValue { get; set; }
+
+        void AddToDoItem()
+        {
+            TodoItems.Add(new TodoItem(NewToDoInputValue, false));
+        }
+    }
+}
